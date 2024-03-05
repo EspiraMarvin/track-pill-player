80e6c07577c7da3c6d5acccd01bc5001188dd5dc
EspiraMarvin <espiraMarvin@gmail.com>
2024-03-05 15:48:42 +0300

    feat: add application data

---
+++ src/data/pills.js
@@ -0,0 +1,87 @@
+// audio files
+import beautiful from './a_beautiful_day.mp3'
+import world from './We_Are_The_World.mp3'
+import top from './dbang-world.mp3'
+import cinematic from './cinematic-time-lapse-115672.mp3'
+import forest from './forest-lullaby-110624.mp3'
+import podcast from './the-podcast-intro-111863.mp3'
+import ambitionwale from './ambitionwale.mp3'
+import epichybrid from './epic-hybrid.mp3'
+import darkengine from './dark-engine.mp3'
+import deepstrange from './deep-strange.mp3'
+
+// audio thumbnails
+import lexin from './lexin.jpeg'
+import dbanj from './dbanj.png'
+import jackson from './jackson.jpeg'
+import trinix from './trinix.jpeg'
+import helix from './helix.jpg'
+import ambition from './ambition.jpeg'
+import noimage from './noimage.png'
+
+export const audioLibraryFiles = [
+  {
+    title: 'Epic Hybrid',
+    src: epichybrid,
+    author: 'Ivan Luzan',
+    thumbnail: noimage,
+  },
+  {
+    title: 'Dark Engine',
+    src: darkengine,
+    author: 'Alex Kizenkov',
+    thumbnail: noimage,
+  },
+  {
+    title: 'Deep Strange',
+    src: deepstrange,
+    author: 'Alko Changing',
+    thumbnail: noimage,
+  },
+  {
+    title: 'Michael Jackson – We Are The World',
+    src: world,
+    author: 'Michael Jackson',
+    thumbnail: jackson,
+  },
+  {
+    title: 'Wale ft Meek Mill, Rick Ross – Ambition',
+    src: ambitionwale,
+    author: 'Wale ft Meek Mill, Rick Ross',
+    thumbnail: ambition,
+  },
+  {
+    title: 'Trinix ft Rushawn – Its a beautiful day',
+    src: beautiful,
+    author: 'Trinix ft Rushawn',
+    thumbnail: trinix,
+  },
+  {
+    title: 'D’banj -Top Of The World',
+    src: top,
+    author: 'Dbanj',
+    thumbnail: dbanj,
+  },
+  {
+    title: 'Helix sound 1',
+    src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+    author: '',
+    thumbnail: helix,
+  },
+  {
+    title: 'Cinematic Time Lapse',
+    src: cinematic,
+    author: 'Lexin Music',
+    thumbnail: lexin,
+  },
+  {
+    title: 'Forest Lullaby',
+    src: forest,
+    author: 'Lesfm',
+  },
+  {
+    title: 'The Podcast Intro',
+    src: podcast,
+    author: 'Music Unlimited',
+  },
+]
